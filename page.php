f78ab7a423f66c753d8feff49284d32236b62866
Emanuele Mossa <48210027+emossa@users.noreply.github.com>
2019-03-30 16:17:26 +0000

    Add files via upload

--- page.php
+++ page.php
@@ -10,16 +10,21 @@
 <div class="content">
 
 	<?php if (have_posts()) :?><?php while(have_posts()) : the_post(); ?>
-
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-
+<div class="content__post__singolo">
+	<article id="post-<?php the_ID(); ?>"  <?php post_class(); ?>>
 
 
 
-			<?php the_content();?>
 
-		</article>
+<?php the_tags('<p>Tags: ', ', ', '</p>'); ?>
+		<?php the_content();?>
+		<?php if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif; ?>
+
+	</article>
+</div>
+
 
 	<?php endwhile; else : ?>
 
